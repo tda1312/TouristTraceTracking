cb6822d6896a0a046a8ad23661a5f13cae41dd13
rubydog1999 <trungdzung9900@gmail.com>
2019-12-17 15:51:51 +0700

    User Interface Login Register UserInfo

--- app/src/main/java/vn/edu/usth/touristtracetracking/UserProfileActivity.java
+++ app/src/main/java/vn/edu/usth/touristtracetracking/UserProfileActivity.java
@@ -2,13 +2,23 @@ package vn.edu.usth.touristtracetracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfileActivity extends AppCompatActivity {
-
+    Button btsave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        Button btsave = findViewById(R.id.bt_save);
+        btsave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(UserProfileActivity.this,MapsActivity.class));
+            }
+        });
     }
 }
